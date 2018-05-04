13b5f68c0dbe31a05ecf14991006d3b59299bee1
frase <frase@192.168.1.8>
2018-05-04 21:22:21 +1200

    Continued working

--- SENG201/src/Lair.java
+++ SENG201/src/Lair.java
@@ -2,18 +2,52 @@ import java.util.Random;
 
 public class Lair implements Location {
 	
+	private String[] names = {"Billy", "Ray", "Ricky", "Bobby", "Joe", "Johnny",
+			"Jimmy", "Jake", "Willy", "Kenny", "Cletus", "Russel", "Renny",
+			"Benny", "Bob", "Steve"}; 
+	
 	private String[] taunts = {"He He He He", "Fe Fi Fo Fum, I advise you to run",
 			"Go away", "I really cannot be bothered dealing with you right now",
 			"Your time is over", "Pasta Lavista baby"};
 	
+	
 	Random r = new Random();
+	
+	MenuSystem m = new MenuSystem();
+	
+	private String villain_name = names[r.nextInt(names.length)];
 	
 	private String villain_taunt = taunts[r.nextInt(taunts.length)];
 
 	@Override
 	public void travelTo(Team team) {
 		
-				
+		m.displayMessage(villain_name + " the Villain says \"" +  villain_taunt
+				+ "\"");
+		
+		String message = "Would you like to enter the Lair of " + villain_name + "?";
+		
+		String[] options = {"Yes", "No"};
+		
+		int result = m.displayMenu(message, options);
+		
+		if (result == 1) {
+			
+			m.displayMessage("Ok. Returning to home base then");
+			
+			return;
+			
+		}
+		
+		String[] heros = team.HeroStrings();
+		
+		if(heros.length > 1) {
+			
+			message = "Which hero is going to fight " + villain_name;
+			
+			m.displayMenu(message, heros);
+			
+		} else
 		
 	}
 
--- SENG201/src/Team.java
+++ SENG201/src/Team.java
@@ -1,4 +1,12 @@
 
 public class Team {
-
+	
+	public String[] HeroStrings() {
+		
+		String[] names = {"Stevie Nicks who has 2HP", "Mick Fleetwood who has 1HP"};
+		
+		return names;
+		
+	}
+	
 }
