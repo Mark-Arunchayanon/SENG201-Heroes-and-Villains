ad6c3c1cf9e20288b7b8a504cca86909c39df1c6
Mark <Mark@192.168.1.72>
2018-05-13 22:21:00 +1200

    Created Healing item class

---
+++ SENG201/src/HealingItem.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+
+public class HealingItem implements Saleable {
+
+	private static final int MAX_HEAL = 20;
+	private static final int MIN_HEAL = 5;
+	private static final int MAX_HEAL_TIME = 90;
+	private static final int MIN_HEAL_TIME = 20;
+	private static final int PRICE_COEFF = 40;
+	private static final int HEAL_CONSTANT = 4;
+	
+	Random num = new Random();
+	
+	private int heal = (num.nextInt(MAX_HEAL - MIN_HEAL) + MIN_HEAL) * HEAL_CONSTANT;
+	private int time = num.nextInt(MAX_HEAL_TIME - MIN_HEAL_TIME) + MIN_HEAL_TIME;
+	private int price = (heal / time) * PRICE_COEFF;
+	
+	@Override
+	public String getSaleDescriptor() {
+		String description = "Healing Potion\nA bottle per hero\nHealth boost: " + heal + "\nHeal time: " + time;
+		
+		return description;
+	}
+
+	@Override
+	public int getPrice() {
+		return price;
+	}
+	
+	public int getTime() {
+		return time;
+	}
+	
+	public int getHeal() {
+		return heal;
+	}
+	
+	public int getHealConstant() {
+		return HEAL_CONSTANT;
+	}
+}
