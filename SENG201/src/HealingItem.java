d39aaa821102598aafc906542ec923b3735ba6cf
Mark <Mark@192.168.1.72>
2018-05-13 20:39:02 +1200

    Testing

---
+++ SENG201/src/HealingItem.java
@@ -0,0 +1,16 @@
+
+public class HealingItem implements Saleable {
+
+	@Override
+	public String getSaleDescriptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
