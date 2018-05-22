5e00ed8eaf65d112c5a85f0053037712010f4181
frase <frase@10.196.122.243>
2018-05-22 23:49:42 +1200

    Attempt 2

--- SENG201/src/DiceRoll.java
+++ SENG201/src/DiceRoll.java
@@ -7,11 +7,10 @@ import java.awt.GridBagConstraints;
 import javax.swing.JLabel;
 import java.awt.Insets;
 import javax.swing.SwingConstants;
-import javax.swing.AbstractAction;
+import javax.swing.SwingUtilities;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.Action;
+import java.awt.Font;
 /**
  * This is a game called Dice Roll, roll a higher number to win
  * Numbers range from 1-6 and it is best out three games
@@ -29,67 +28,75 @@ public class DiceRoll implements VillainGame {
 	MenuSystem m;
 	Random num = new Random();
 	private Object synchronizer = new Object();
+	private JLabel lab_1;
+	private JLabel lab_2;
+	private JLabel lab_3;
+	private JLabel lab_4;
+	private String lab2;
+	private String lab3;
+	private String lab4;
+	
 	
 	
 	public DiceRoll(MenuSystem m) {
 		GridBagLayout gbl_panel = new GridBagLayout();
 		gbl_panel.columnWidths = new int[]{0};
-		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
 		gbl_panel.columnWeights = new double[]{1};
-		gbl_panel.rowWeights = new double[]{1.0, 2.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.5, 1.0, 1.0, 0.5, 0.5, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
-		JLabel lab_1 = new JLabel("New");
+		lab_1 = new JLabel("Roll the Dice");
+		lab_1.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lab_1.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_lab_1 = new GridBagConstraints();
-		gbc_lab_1.anchor = GridBagConstraints.SOUTHWEST;
+		gbc_lab_1.fill = GridBagConstraints.BOTH;
 		gbc_lab_1.insets = new Insets(0, 0, 5, 0);
 		gbc_lab_1.gridx = 0;
 		gbc_lab_1.gridy = 0;
 		panel.add(lab_1, gbc_lab_1);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setVerticalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.anchor = GridBagConstraints.SOUTHWEST;
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
-		gbc_lblNewLabel_1.gridx = 0;
-		gbc_lblNewLabel_1.gridy = 1;
-		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		lab_2 = new JLabel("");
+		lab_2.setVerticalAlignment(SwingConstants.CENTER);
+		lab_2.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_lab_2 = new GridBagConstraints();
+		gbc_lab_2.fill = GridBagConstraints.BOTH;
+		gbc_lab_2.insets = new Insets(0, 0, 5, 0);
+		gbc_lab_2.gridx = 0;
+		gbc_lab_2.gridy = 1;
+		panel.add(lab_2, gbc_lab_2);
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton btnNewButton = new JButton("Click to roll the dice");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				synchronized(synchronizer) {
 					//TODO try except
-					try {
-						int temp_guess = Integer.parseInt(txtType.getText());
-						if (temp_guess < 11 && temp_guess > 0) guess = temp_guess;
-						synchronizer.notify();
-					} catch (NumberFormatException f) {
-						// Player is notified to only type integers in the text box
-					}
-					
-					
+					synchronizer.notify();
 				}
 			}
 		});
+		
+		lab_3 = new JLabel("");
+		GridBagConstraints gbc_lab_3 = new GridBagConstraints();
+		gbc_lab_3.insets = new Insets(0, 0, 5, 0);
+		gbc_lab_3.gridx = 0;
+		gbc_lab_3.gridy = 2;
+		panel.add(lab_3, gbc_lab_3);
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
 		gbc_btnNewButton.gridx = 0;
-		gbc_btnNewButton.gridy = 2;
+		gbc_btnNewButton.gridy = 3;
 		panel.add(btnNewButton, gbc_btnNewButton);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 3;
-		panel.add(lblNewLabel, gbc_lblNewLabel);
+		lab_4 = new JLabel("This is a best of three games of roll the dice, roll a higher number to win");
+		GridBagConstraints gbc_lab_4 = new GridBagConstraints();
+		gbc_lab_4.gridx = 0;
+		gbc_lab_4.gridy = 4;
+		panel.add(lab_4, gbc_lab_4);
 		
 		this.m = m;
 		
-		
+		m.updatePanel(panel);
 	}
 
 	@Override
@@ -102,28 +109,42 @@ public class DiceRoll implements VillainGame {
 		int villain_roll = 0;
 		int illusion = playing_hero.getIllusion();
 		
-		m.displayMessage("This is a best of three games of roll the dice, roll a higher number to win");
-		
 		int[] options = {1, 2, 3, 4, 5, 6};
 		
 		// Runs the game until one of the player's score reaches 2. Press 1 and enter to roll, other input will not be accepted.
 		while(player_score < 2 && villain_score < 2) {
 			
-			hero_roll = num.nextInt(6);
-			villain_roll = num.nextInt(6);
-			m.displayMessage(playing_hero.getName() + " rolled a " + options[hero_roll] + " and " + villain_name + "rolled a " + options[villain_roll]);
-			if (hero_roll > villain_roll) {
-				player_score++;
-				m.displayMessage(playing_hero.getName() + " rolled a higher number");
-			} else if (hero_roll < villain_roll) {
-				villain_score++;
-				m.displayMessage(villain_name + " rolled a higher number");
-			} else if (hero_roll == villain_roll) { 
-				m.displayMessage(playing_hero.getName() + " and " + villain_name + " rolled the same number");
+			synchronized(synchronizer) {
+				try {
+					synchronizer.wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 			
-			m.displayMessage("Current scores are");
-			m.displayMessage(playing_hero.getName() + ": " + player_score + "\n" + villain_name + ": " + villain_score);
+			hero_roll = num.nextInt(6);
+			villain_roll = num.nextInt(6);
+			lab2 = playing_hero.getName() + " rolled a " + options[hero_roll] + " and " + villain_name + " rolled a " + options[villain_roll];
+			if (hero_roll > villain_roll) {
+				player_score++;
+				lab3 = playing_hero.getName() + " rolled a higher number";
+			} else if (hero_roll < villain_roll) {
+				villain_score++;
+				lab3 = villain_name + " rolled a higher number";
+			} else if (hero_roll == villain_roll) { 
+				lab3 = playing_hero.getName() + " and " + villain_name + " rolled the same number";
+			}
+			
+			lab4 = "Current scores are " + playing_hero.getName() + ": " + player_score + ", " + villain_name + ": " + villain_score;
+			
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					lab_2.setText(lab2);
+					lab_3.setText(lab3);
+					lab_4.setText(lab4);
+				}
+			});
 		}
 		
 		
@@ -132,12 +153,25 @@ public class DiceRoll implements VillainGame {
 		
 		// Announce winner or loser. If Hero was defeated, the illusion skill may work and change the defeat into a win
 		if (player_score == 2) {
-			m.displayMessage("Congratualtions. " + playing_hero.getName() + " beat " + villain_name);
+			lab2 = "Congratualtions. " + playing_hero.getName() + " beat " + villain_name;
+		} else {
+			lab2 = "Unfortunately, " + playing_hero.getName() + " lost this game";
+			if (ran_chance >= win_chance) {
+				lab3 = "But " + playing_hero.getName() + " useds illusion skill to trick " + villain_name + " into winning the game";
+			}
+		}
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				lab_2.setText(lab2);
+				lab_3.setText(lab3);
+			}
+		});
+		
+		if (player_score == 2) {
 			return false;
 		} else {
-			m.displayMessage("Unfortunately, " + playing_hero.getName() + " lost this game");
 			if (ran_chance >= win_chance) {
-				m.displayMessage("But " + playing_hero.getName() + " useds illusion skill to trick " + villain_name + " into winning the game");
 				return false;
 			} else {
 				return true;
@@ -150,13 +184,4 @@ public class DiceRoll implements VillainGame {
 		return ("Dice Roll, roll a higher number");
 	}
 
-
-	private class SwingAction extends AbstractAction {
-		public SwingAction() {
-			putValue(NAME, "SwingAction");
-			putValue(SHORT_DESCRIPTION, "Some short description");
-		}
-		public void actionPerformed(ActionEvent e) {
-		}
-	}
 }
